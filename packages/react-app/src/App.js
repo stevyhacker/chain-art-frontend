50c517898d6337fefa30efbf27111887c4483642
Stevan Bogosavljevic <stevyhacker@gmail.com>
2022-01-12 12:50:09 +0100

    Update description

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -164,9 +164,13 @@ function App() {
                     <h2>On-chain Art NFT Minter (⛓,🖼)</h2>
 
                     <p className={"description"} style={{marginBottom: "50px"}}>
-                        Upload your image here and confirm the transaction.
+                        Why on-chain minting?
                         <br/>
-                        The image will be saved on-chain in base64 encoding and an NFT will be minted for you.
+                        Because NFTs today store data on servers or IPFS which are in no way related to the blockchain which hosts NFTs.
+                        <br/>
+                        Here the images will be saved on-chain in base64 encoding and an NFT will be minted for you.
+                        <br/>
+                        You can get the metadata and art straight from Polygonscan.
                         <br/>
                         <br/>
                         Image file size is limited to under 20 kb right now.
@@ -179,6 +183,11 @@ function App() {
                         Other formats will take more space and require more gas == more expensive transactions.
                         <br/>
                         Protocol has a minting fee of 1 MATIC per NFT.
+                        <br/>
+                        Upload your image here and confirm the transaction.
+                        <br/>
+                        After the transaction is mined you can see the NFT on your wallet on OpenSea.
+
                     </p>
 
                     <ImageUploading
